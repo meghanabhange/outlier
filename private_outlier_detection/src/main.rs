275299898b7e0bcc68d974aad2eea1c8282f272d
meghanabhange <meghanabhange13@gmail.com>
2022-09-10 00:36:22 +0530

    update

--- private_outlier_detection/src/main.rs
+++ private_outlier_detection/src/main.rs
@@ -1,27 +1,28 @@
+use ciphercore_base::data_types::{array_type, Type, INT64};
+use ciphercore_base::errors::Result;
+use ciphercore_base::graphs::create_context;
+
+use crate::linear::LinearRegression;
+
 mod linear;
 
-use ciphercore_base::graphs::create_context;
-use ciphercore_base::data_types::{array_type, INT64, Type};
-use ciphercore_base::errors::Result;
-
 fn main() -> Result<()> {
-        let context = create_context()?;
-        let graph = context.create_graph()?;
+    let context = create_context()?;
+    let graph = context.create_graph()?;
 
+    let t: Type = array_type(vec![3, 3], INT64);
+    let y = array_type(vec![3, 1], INT64);
+    let x = graph.input(t.clone()).unwrap();
+    let y = graph.input(y.clone()).unwrap();
 
-        let t: Type = array_type(vec![3, 3], INT64);
-        let y = array_type(vec![3, 1], INT64);
-        let x = graph.input(t.clone()).unwrap();
-        let y = graph.input(y.clone()).unwrap();
+    let linear_regression = LinearRegression::new(&graph, x, y);
+    let coefficients = linear_regression.coefficients;
 
+    graph.set_output_node(coefficients)?;
 
-        let coefficients = linear::linear_regression(&graph, x, y);
-
-        graph.set_output_node(coefficients)?;
-
-        graph.finalize()?;
-        context.set_main_graph(graph)?;
-        context.finalize()?;
-        println!("{}", serde_json::to_string(&context)?);
-        Ok(())
-    }
+    graph.finalize()?;
+    context.set_main_graph(graph)?;
+    context.finalize()?;
+    println!("{}", serde_json::to_string(&context)?);
+    Ok(())
+}
